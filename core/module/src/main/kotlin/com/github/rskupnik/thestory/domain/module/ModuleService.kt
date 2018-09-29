a8cd286c7715119d3396d55295d6c05bd1733c8d
Myzreal <r.skupnik@gmail.com>
2018-09-29 14:02:04 +0200

    Introduce module component and interface

---
+++ core/module/src/main/kotlin/com/github/rskupnik/thestory/domain/module/ModuleService.kt
@@ -0,0 +1,18 @@
+package com.github.rskupnik.thestory.domain.module
+
+import com.github.rskupnik.thestory.shared.Reference
+import com.github.rskupnik.thestory.shared.external.Image
+import com.github.rskupnik.thestory.shared.persistence.PersistableProvider
+import com.github.rskupnik.thestory.shared.persistence.PersistableState
+
+interface ModuleService : PersistableProvider<PersistableState> {
+
+    fun load(module: String): List<Reference>
+
+    //fun getLoadedModules(): List<ModuleView>
+
+    //fun getLoadedStandaloneModule(): ModuleView?
+
+    // TODO: Should this be here or in a separate component that deals with assets?
+    fun getImage(ref: Reference): Image?
+}
