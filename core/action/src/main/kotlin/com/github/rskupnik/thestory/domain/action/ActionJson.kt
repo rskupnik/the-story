227776a7de5a9ca1335928aa7de92d3c1760404f
Myzreal <r.skupnik@gmail.com>
2018-09-30 14:04:51 +0200

    Add action json class

---
+++ core/action/src/main/kotlin/com/github/rskupnik/thestory/domain/action/ActionJson.kt
@@ -0,0 +1,12 @@
+package com.github.rskupnik.thestory.domain.action
+
+import com.github.rskupnik.thestory.shared.json.DefinitionJsonRepresentation
+
+data class ActionJson(
+        val id: String,
+        val contexts: List<String>,
+        val params: Map<String, Any>,
+        val conditions: Map<String, Any>
+) : DefinitionJsonRepresentation<Action> {
+    override fun toDefinition(): Action = Action(id, contexts, params, conditions)
+}
