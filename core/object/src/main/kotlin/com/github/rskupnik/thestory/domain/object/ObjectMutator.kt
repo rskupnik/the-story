42019591f4437d0bf5161ba35331ac188b95f88b
Myzreal <r.skupnik@gmail.com>
2018-10-03 19:54:53 +0200

    Add ObjectMutator

---
+++ core/object/src/main/kotlin/com/github/rskupnik/thestory/domain/object/ObjectMutator.kt
@@ -0,0 +1,24 @@
+package com.github.rskupnik.thestory.domain.`object`
+
+import com.github.rskupnik.thestory.domain.`object`.internal.ObjectInstance
+import com.github.rskupnik.thestory.shared.Mutator
+import com.github.rskupnik.thestory.shared.MutatorEntry
+
+class ObjectMutator private constructor(
+        externalState: Map<String, Any>?
+) : Mutator() {
+
+    private val externalStateMutator: MutatorEntry<Map<String, Any>> = set(externalState)
+
+    internal fun mutate(obj: ObjectInstance) {
+        if (externalStateMutator.enabled)
+            obj.externalState = mutateState(obj.externalState, externalStateMutator.value ?: return)
+    }
+
+    data class Builder(
+            var externalState: Map<String, Any>? = null
+    ) {
+        fun externalState(externalState: Map<String, Any>) = apply { this.externalState = externalState }
+        fun build() = ObjectMutator(externalState)
+    }
+}
