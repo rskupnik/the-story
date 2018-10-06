ca64ebb15123396ebcc418ea3a7fb510ec66b9a5
Myzreal <r.skupnik@gmail.com>
2018-10-06 13:50:02 +0200

    Add some kdocs

--- core/location/src/main/kotlin/com/github/rskupnik/thestory/domain/LocationId.kt
+++ core/location/src/main/kotlin/com/github/rskupnik/thestory/domain/LocationId.kt
@@ -1,10 +1,22 @@
 package com.github.rskupnik.thestory.domain
 
+/**
+ * A ValueObject that makes it easy to pass around location identifiers, as locations
+ * are identified by the zone they're in and an a pair of x + y coordinates.
+ */
 data class LocationId(
         val zone: String,
         val x: Int,
         val y: Int
 ) {
+
+    /**
+     * Produce a new [LocationId] pointing to a location relative to this location.
+     *
+     * @param direction the [Direction] to be applied
+     * @return a [LocationId] being the result of applying [direction] to
+     * this location
+     */
     fun applyDirection(direction: Direction): LocationId = LocationId(
             zone,
             x + (if (direction == Direction.WEST) -1 else if (direction == Direction.EAST) 1 else 0),
