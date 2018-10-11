16bbdc5182b83bedbd6b6969f0a5ef9a8246ec91
Myzreal <r.skupnik@gmail.com>
2018-10-11 20:41:05 +0200

    Try to make this thing launchable (failed)

--- glue/src/main/kotlin/com/github/rskupnik/thestory/application/Application.kt
+++ glue/src/main/kotlin/com/github/rskupnik/thestory/application/Application.kt
@@ -1,7 +1,9 @@
 package com.github.rskupnik.thestory.application
 
+import com.github.rskupnik.thestory.application.internal.Injector
+
 object Application {
     fun init() {
-
+        val injector: Injector = DaggerInjector.create()
     }
 }
