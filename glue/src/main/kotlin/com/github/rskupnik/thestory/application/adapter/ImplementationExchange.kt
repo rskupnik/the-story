2afd07cd410fc2dace897de70a86c733744610ca
Myzreal <r.skupnik@gmail.com>
2018-10-15 20:01:46 +0200

    Fix the issue with implementation not provided

--- glue/src/main/kotlin/com/github/rskupnik/thestory/application/adapter/ImplementationExchange.kt
+++ glue/src/main/kotlin/com/github/rskupnik/thestory/application/adapter/ImplementationExchange.kt
@@ -1,13 +1,17 @@
 package com.github.rskupnik.thestory.application.adapter
 
+import com.github.rskupnik.thestory.shared.external.CallbackReceiver
 import com.github.rskupnik.thestory.shared.external.Port
+import com.github.rskupnik.thestory.shared.external.asset.AssetLoader
 import com.github.rskupnik.thestory.shared.external.file.FileLoader
 import kotlin.reflect.KClass
 
 internal class ImplementationExchange : ImplementationProvider, ImplementationSupplier {
 
     private val delegates: Map<KClass<out Port>, ImplementationDelegate<out Port>> = mapOf(
-        FileLoader::class to FileLoaderDelegate()
+        FileLoader::class to FileLoaderDelegate(),
+        AssetLoader::class to AssetLoaderDelegate(),
+        CallbackReceiver::class to CallbackReceiverDelegate()
     )
 
     override fun <T : Port> provideImplementation(klass: KClass<T>, implementation: T) {
