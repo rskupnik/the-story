5576f5e5e2c045bfd013c70e8243a70e9c5b71e1
Myzreal <r.skupnik@gmail.com>
2018-10-17 20:47:38 +0200

    Make test code load from classpath for testing

--- test/src/main/kotlin/com/github/rskupnik/thestory/main/Main.kt
+++ test/src/main/kotlin/com/github/rskupnik/thestory/main/Main.kt
@@ -8,51 +8,56 @@ import com.github.rskupnik.thestory.shared.external.asset.ImageProvider
 import com.github.rskupnik.thestory.shared.external.dto.OptionLabel
 import com.github.rskupnik.thestory.shared.external.file.FileHandle
 import com.github.rskupnik.thestory.shared.external.file.FileLoader
+import java.io.File
 import java.io.InputStream
+import java.nio.file.Files
 
 fun main(args: Array<String>) {
     // For ad-hoc testing
     val api = Application.init()
-    api.provideImplementation(AssetLoader::class, DummyAssetLoader())
-    api.provideImplementation(CallbackReceiver::class, CallbackReceiverImpl())
-    api.provideImplementation(FileLoader::class, DummyFileLoader())
+    api.provideImplementation(AssetLoader::class, Main.DummyAssetLoader())
+    api.provideImplementation(CallbackReceiver::class, Main.CallbackReceiverImpl())
+    api.provideImplementation(FileLoader::class, Main.DummyFileLoader())
     api.getCommandAPI().initializeGame("demo")
     //val availableModules = api.getQueryAPI().getAvailableModules()
     //availableModules.availableModules.forEach(::println)
 }
 
-class CallbackReceiverImpl : CallbackReceiver {
-    override fun onDisplayOptions(options: List<OptionLabel>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
+class Main {
 
-class DummyAssetLoader : AssetLoader {
-    override fun loadImageProvider(fileHandle: FileHandle): ImageProvider {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
-
-class DummyFileLoader : FileLoader {
-    override fun getFileHandle(path: String): FileHandle? = DummyFileHandle(ClassLoader.getSystemClassLoader().getResourceAsStream(path))
-
-    override fun getHandleToModule(module: String): FileHandle? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class CallbackReceiverImpl : CallbackReceiver {
+        override fun onDisplayOptions(options: List<OptionLabel>) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 
-    override fun loadAsString(fileHandle: FileHandle): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class DummyAssetLoader : AssetLoader {
+        override fun loadImageProvider(fileHandle: FileHandle): ImageProvider {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 
-    override fun loadAsImage(fileHandle: FileHandle): Image {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class DummyFileLoader : FileLoader {
+        override fun getFileHandle(path: String): FileHandle? = DummyFileHandle(Main::class.java.classLoader.getResourceAsStream(path))
+
+        override fun getHandleToModule(module: String): FileHandle? {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun loadAsString(fileHandle: FileHandle): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun loadAsImage(fileHandle: FileHandle): Image {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun loadSnapshot(filename: String): Map<String, Any>? {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 
-    override fun loadSnapshot(filename: String): Map<String, Any>? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class DummyFileHandle(val inputStream: InputStream) : FileHandle {
+
     }
-}
-
-class DummyFileHandle(val inputStream: InputStream) : FileHandle {
-
 }
