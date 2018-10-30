97c935eebb7e740ef70ccf1dc487433066577806
Myzreal <r.skupnik@gmail.com>
2018-10-30 20:09:43 +0100

    Inherit Event

--- core/equipment/event/src/main/kotlin/com/github/rskupnik/thestory/equipment/event/ItemEquippedEvent.kt
+++ core/equipment/event/src/main/kotlin/com/github/rskupnik/thestory/equipment/event/ItemEquippedEvent.kt
@@ -1,5 +1,6 @@
 package com.github.rskupnik.thestory.equipment.event
 
+import com.github.rskupnik.thestory.event.Event
 import com.github.rskupnik.thestory.shared.Reference
 
-data class ItemEquippedEvent(val item: Reference)
+data class ItemEquippedEvent(val item: Reference) : Event
