b7e17ea9142d66e66109bd0ad2eccbfa713e278e
Radosław Skupnik <rskupnik@gmail.com>
2018-11-08 14:55:42 +0100

    Add a basic test module with dummy implementation

---
+++ test/src/test/groovy/com/github/rskupnik/thestory/HelloWorldSpec.groovy
@@ -0,0 +1,16 @@
+package com.github.rskupnik.thestory
+
+import spock.lang.Specification
+
+class HelloWorldSpec extends Specification {
+    def "length of Spock's and his friends' names"() {
+        expect:
+        name.size() == length
+
+        where:
+        name     | length
+        "Spock"  | 5
+        "Kirk"   | 4
+        "Scotty" | 6
+    }
+}
