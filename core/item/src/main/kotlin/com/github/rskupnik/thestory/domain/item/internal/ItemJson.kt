5ae231f92afaca80237f3c50f268b0fd9997bc55
Myzreal <r.skupnik@gmail.com>
2018-11-09 20:21:15 +0100

    Separate options in ItemJson

--- core/item/src/main/kotlin/com/github/rskupnik/thestory/domain/item/internal/ItemJson.kt
+++ core/item/src/main/kotlin/com/github/rskupnik/thestory/domain/item/internal/ItemJson.kt
@@ -2,6 +2,7 @@ package com.github.rskupnik.thestory.domain.item.internal
 
 import com.github.rskupnik.thestory.core.callback.domain.CallbackJson
 import com.github.rskupnik.thestory.option.domain.OptionJson
+import com.github.rskupnik.thestory.shared.ExternalState
 import com.github.rskupnik.thestory.shared.Reference
 import com.github.rskupnik.thestory.shared.json.BlueprintJsonRepresentation
 import com.github.rskupnik.thestory.shared.json.DefinitionJsonRepresentation
@@ -11,17 +12,17 @@ internal data class ItemJson(
         val label: String,
         val image: String,
         val type: String?,
-        val initialState: Map<String, Any>,
-        val options: List<OptionJson>,
-        val callbacks: List<CallbackJson>
+        val initialState: Map<String, Any>?,
+        val options: List<OptionJson>?,
+        val callbacks: List<CallbackJson>?
 ) : BlueprintJsonRepresentation<ItemBlueprint> {
     override fun toBlueprint(): ItemBlueprint = ItemBlueprint(
             id,
             label,
             Reference.to(image),
             if (type != null) ItemBlueprint.Type.valueOf(type) else ItemBlueprint.Type.REGULAR,
-            initialState,
-            DefinitionJsonRepresentation.convertList(options),
-            DefinitionJsonRepresentation.convertList(callbacks)
+            ExternalState.fromExistingState(initialState),
+            if (options != null) DefinitionJsonRepresentation.convertList(options) else emptyList(),
+            if (callbacks != null) DefinitionJsonRepresentation.convertList(callbacks) else emptyList()
     )
 }
