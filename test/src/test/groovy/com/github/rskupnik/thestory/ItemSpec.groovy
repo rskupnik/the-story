71b43193b57b10b7e67262cfb326d1e060f745de
Radosław Skupnik <rskupnik@gmail.com>
2018-11-10 13:41:00 +0100

    Add ItemSpec (empty) and MovementSpec (basic)

---
+++ test/src/test/groovy/com/github/rskupnik/thestory/ItemSpec.groovy
@@ -0,0 +1,8 @@
+package com.github.rskupnik.thestory
+
+import spock.lang.Specification
+
+class ItemSpec extends Specification {
+
+
+}
---
+++ test/src/test/groovy/com/github/rskupnik/thestory/MovementSpec.groovy
@@ -0,0 +1,24 @@
+package com.github.rskupnik.thestory
+
+import com.github.rskupnik.thestory.application.API
+import com.github.rskupnik.thestory.external.feedback.CallbackReceiver
+import com.github.rskupnik.thestory.implementations.proxy.ProxyCallbackReceiver
+import com.github.rskupnik.thestory.setup.Setup
+import com.github.rskupnik.thestory.shared.Direction
+import spock.lang.Specification
+
+class MovementSpec extends Specification {
+
+    def "should be able to move"() {
+        given:
+        def (API api, CallbackReceiver callbackReceiverProxy) = Setup.standardApplication()
+        def CallbackReceiver callbackReceiver = Mock()
+        ((ProxyCallbackReceiver)callbackReceiverProxy).setActual(callbackReceiver)
+
+        when:
+        api.commandAPI.movePlayer(Direction.WEST)
+
+        then:
+        1 * callbackReceiver.onLocationLoaded(_)
+    }
+}
