db43f6b93ae1f7accded028f83560f84f2eb7beb
Myzreal <r.skupnik@gmail.com>
2018-11-11 20:49:30 +0100

    Introduce StateVerifier

--- test/src/test/groovy/com/github/rskupnik/thestory/PersistenceSpec.groovy
+++ test/src/test/groovy/com/github/rskupnik/thestory/PersistenceSpec.groovy
@@ -1,12 +1,14 @@
 package com.github.rskupnik.thestory
 
 import com.github.rskupnik.thestory.external.feedback.CallbackReceiver
+import com.github.rskupnik.thestory.external.file.FileSaver
 import com.github.rskupnik.thestory.implementations.inmemory.InMemoryFileSaver
 import com.github.rskupnik.thestory.setup.ApplicationContext
 import com.github.rskupnik.thestory.shared.Direction
 import com.github.rskupnik.thestory.shared.ExternalState
 import com.github.rskupnik.thestory.shared.entity.EntityType
 
+// TODO: Running both fails
 class PersistenceSpec extends AbstractSpec {
 
     def "should save state - basic test"() {
@@ -36,24 +38,49 @@ class PersistenceSpec extends AbstractSpec {
         app.api.commandAPI.saveGame()
 
         then:
-        // TODO: Pull out verification code to a separate class?
-        Map<String, ?> savedState = ((InMemoryFileSaver)app.fileSaver).get("demo.sav") as Map<String, ?>
-        savedState != null
-        List<Map<String, ?>> itemsState = savedState.get("items")
-        itemsState != null
-        itemsState.size() == 1
-        Map<String, ?> item = itemsState.get(0)
-        item != null
-        item.get("id") != null
-        ((String)item.get("id")).length() > 10
-        item.get("blueprint") != null
-        ((String)item.get("blueprint")).equals("torch")
-        item.get("externalState") != null
-        ((ExternalState)item.get("externalState")).size() == 1
-        ((ExternalState)item.get("externalState")).get("toggled") == false
-        item.get("currentImage") == null
-        item.get("placement") == null
+        def verifier = new SavedStateVerifier(app.fileSaver, "demo.sav")
+        verifier.verifyExists()
+        verifier.verifyItemAmount(1)
+        verifier.verifyItem(0, "torch", [toggled: false])
+    }
+
+    private boolean verifyStateExists(FileSaver fileSaver, String filename) {
+        return ((InMemoryFileSaver)fileSaver).get(filename) != null
     }
 
     // TODO: Test - click object displays list of options
+
+    // TODO: Pull this class out
+    private class SavedStateVerifier {
+        private final Map<String, ?> state
+
+        private SavedStateVerifier(FileSaver fileSaver, String filename) {
+            this.state = ((InMemoryFileSaver)fileSaver).get(filename) as Map<String, ?>
+        }
+
+        private boolean verifyExists() {
+            return state != null
+        }
+
+        private boolean verifyItemAmount(int amount) {
+            List<Map<String, ?>> items = state.get("items")
+            return items != null && items.size() == amount
+        }
+
+        private boolean verifyItem(int index, String blueprint, Map<String, ?> externalState) {
+            List<Map<String, ?>> items = state.get("items")
+            Map<String, ?> state = items.get(index)
+            return ((String)state.get("id")).length() > 10 && ((String)state.get("blueprint")).equals(blueprint) &&
+                    verifyState((Map<String, ?>)state.get("externalState"), externalState)
+        }
+
+        private boolean verifyState(Map<String, ?> expected, Map<String, ?> given) {
+            for (Map.Entry<String, ?> entry : given) {
+                if (expected.get(entry.key) == null || !entry.value.equals(expected.get(entry.key))) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
 }
