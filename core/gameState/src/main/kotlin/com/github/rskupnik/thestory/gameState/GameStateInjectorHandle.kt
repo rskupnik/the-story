da03935c2ad4ecbde3507adde52e652c9cfeaa48
Radosław Skupnik <rskupnik@gmail.com>
2018-11-15 14:56:47 +0100

    Update injector handle and module accordingly

--- core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/GameStateInjectorHandle.kt
+++ core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/GameStateInjectorHandle.kt
@@ -1,11 +1,8 @@
 package com.github.rskupnik.thestory.gameState
 
 import com.github.rskupnik.thestory.gameState.internal.DefaultGameStateService
-import com.github.rskupnik.thestory.persistence.PersistenceSubscriber
 
 object GameStateInjectorHandle {
 
-    fun service(
-            persistenceSubscriber: PersistenceSubscriber
-    ): GameStateService = DefaultGameStateService(persistenceSubscriber)
+    fun service(): GameStateService = DefaultGameStateService()
 }
--- glue/src/main/kotlin/com/github/rskupnik/thestory/application/modules/GameStateModule.kt
+++ glue/src/main/kotlin/com/github/rskupnik/thestory/application/modules/GameStateModule.kt
@@ -2,7 +2,6 @@ package com.github.rskupnik.thestory.application.modules
 
 import com.github.rskupnik.thestory.gameState.GameStateInjectorHandle
 import com.github.rskupnik.thestory.gameState.GameStateService
-import com.github.rskupnik.thestory.persistence.PersistenceSubscriber
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -11,7 +10,5 @@ import javax.inject.Singleton
 internal class GameStateModule {
 
     @Provides @Singleton
-    fun service(
-            persistenceSubscriber: PersistenceSubscriber
-    ): GameStateService = GameStateInjectorHandle.service(persistenceSubscriber)
+    fun service(): GameStateService = GameStateInjectorHandle.service()
 }
