147e609f16884fa09209c02c8411e393cd377fb2
Radosław Skupnik <rskupnik@gmail.com>
2018-11-15 14:42:27 +0100

    Remove GameStateService from background module

--- glue/src/main/kotlin/com/github/rskupnik/thestory/application/modules/BackgroundModule.kt
+++ glue/src/main/kotlin/com/github/rskupnik/thestory/application/modules/BackgroundModule.kt
@@ -4,7 +4,6 @@ import com.github.rskupnik.thestory.background.BackgroundInjectorHandle
 import com.github.rskupnik.thestory.background.BackgroundService
 import com.github.rskupnik.thestory.domain.module.ModuleService
 import com.github.rskupnik.thestory.external.feedback.CallbackReceiver
-import com.github.rskupnik.thestory.gameState.GameStateService
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -14,10 +13,9 @@ internal class BackgroundModule {
 
     @Provides @Singleton
     fun service(
-            gameStateService: GameStateService,
             moduleService: ModuleService,
             callbackReceiver: CallbackReceiver
     ): BackgroundService = BackgroundInjectorHandle.service(
-            gameStateService, moduleService, callbackReceiver
+            moduleService, callbackReceiver
     )
 }
