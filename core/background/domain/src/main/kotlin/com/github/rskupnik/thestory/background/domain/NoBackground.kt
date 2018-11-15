c5b7dadde9bd27b42b7000b2d5fae3a07c8ce5fc
Radosław Skupnik <rskupnik@gmail.com>
2018-11-15 13:10:15 +0100

    Adjust NoBackground to return null state

--- core/background/domain/src/main/kotlin/com/github/rskupnik/thestory/background/domain/NoBackground.kt
+++ core/background/domain/src/main/kotlin/com/github/rskupnik/thestory/background/domain/NoBackground.kt
@@ -4,7 +4,7 @@ import com.github.rskupnik.thestory.persistence.Persistable
 
 class NoBackground: Background, Persistable {
 
-    override fun toPersistableState(): Map<String, Any?> = null
+    override fun toPersistableState(): Map<String, Any?>? = null
 
     override fun clone(): Background = NoBackground()
 }
