08803f3dd197d43446533feec3504dc02bb85525
Radosław Skupnik <rskupnik@gmail.com>
2018-11-15 13:08:53 +0100

    Background classes implement Persistable

--- core/background/domain/src/main/kotlin/com/github/rskupnik/thestory/background/domain/NoBackground.kt
+++ core/background/domain/src/main/kotlin/com/github/rskupnik/thestory/background/domain/NoBackground.kt
@@ -1,5 +1,10 @@
 package com.github.rskupnik.thestory.background.domain
 
-class NoBackground: Background {
+import com.github.rskupnik.thestory.persistence.Persistable
+
+class NoBackground: Background, Persistable {
+
+    override fun toPersistableState(): Map<String, Any?> = null
+
     override fun clone(): Background = NoBackground()
 }
--- core/background/domain/src/main/kotlin/com/github/rskupnik/thestory/background/domain/NormalMappedBackground.kt
+++ core/background/domain/src/main/kotlin/com/github/rskupnik/thestory/background/domain/NormalMappedBackground.kt
@@ -1,12 +1,20 @@
 package com.github.rskupnik.thestory.background.domain
 
 import com.github.rskupnik.thestory.external.asset.Image
+import com.github.rskupnik.thestory.persistence.Persistable
 
 data class NormalMappedBackground(
         private val image: Image,
         private val normalImage: Image,
         private val imageStr: String,
         private val normalImageStr: String
-): Background {
+): Background, Persistable {
+
+    override fun toPersistableState(): Map<String, Any?> = mapOf(
+            "type" to "NORMAL_MAPPED",
+            "image" to imageStr,
+            "normalImage" to normalImageStr
+    )
+
     override fun clone(): Background = NormalMappedBackground(image, normalImage, imageStr, normalImageStr)
 }
