0259f02ee31adf4753782bdcc5c6c3e3a2e08d7e
Radosław Skupnik <rskupnik@gmail.com>
2018-11-15 14:54:59 +0100

    Remove everything from GameState classes

--- core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/GameStateService.kt
+++ core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/GameStateService.kt
@@ -1,11 +1,3 @@
 package com.github.rskupnik.thestory.gameState
 
-import com.github.rskupnik.thestory.background.domain.Background
-import com.github.rskupnik.thestory.persistence.Persister
-
-interface GameStateService : Persister {
-
-    fun setBackground(background: Background?)
-
-    fun getBackground(): Background?
-}
+interface GameStateService
--- core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/internal/GameState.kt
+++ core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/internal/GameState.kt
@@ -1,8 +1,3 @@
 package com.github.rskupnik.thestory.gameState.internal
 
-import com.github.rskupnik.thestory.background.domain.Background
-import com.github.rskupnik.thestory.background.domain.NoBackground
-
-internal class GameState {
-    internal var background: Background? = NoBackground()
-}
+internal class GameState
