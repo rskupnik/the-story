ee1b6c79a066dc7497cc0471aa9f31dc77b0199c
Radosław Skupnik <rskupnik@gmail.com>
2018-11-15 13:27:31 +0100

    Add Details interface and package

---
+++ api/src/main/kotlin/com/github/rskupnik/thestory/api/command/details/Details.kt
@@ -0,0 +1,11 @@
+package com.github.rskupnik.thestory.api.command.details
+
+/**
+ * A marker interface for object of type [Details]
+ *
+ * A [Details] object accompanies a command and only serves as a mean of providing
+ * detailed parameters necessary to carry out the command.
+ *
+ * Example: when setting background, we need details on what type of background to set, etc.
+ */
+interface Details
