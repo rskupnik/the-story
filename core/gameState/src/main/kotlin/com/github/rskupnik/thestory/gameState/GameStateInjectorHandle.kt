6ed6616355c3c943b889f3dcbf8b56e37ea14e94
Radosław Skupnik <rskupnik@gmail.com>
2018-11-15 13:15:36 +0100

    Update the injector handle

--- core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/GameStateInjectorHandle.kt
+++ core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/GameStateInjectorHandle.kt
@@ -1,8 +1,11 @@
 package com.github.rskupnik.thestory.gameState
 
 import com.github.rskupnik.thestory.gameState.internal.DefaultGameStateService
+import com.github.rskupnik.thestory.persistence.PersistenceSubscriber
 
 object GameStateInjectorHandle {
 
-    fun service(): GameStateService = DefaultGameStateService()
+    fun service(
+            persistenceSubscriber: PersistenceSubscriber
+    ): GameStateService = DefaultGameStateService(persistenceSubscriber)
 }
