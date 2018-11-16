57bf4c74f37c94afea9feea1f08c4444c8382484
rskupnik <rskupnik@pgs-soft.com>
2018-11-16 13:58:17 +0100

    Update test setup to reflect new changes

--- test/src/test/groovy/com/github/rskupnik/thestory/setup/ApplicationContext.groovy
+++ test/src/test/groovy/com/github/rskupnik/thestory/setup/ApplicationContext.groovy
@@ -1,6 +1,7 @@
 package com.github.rskupnik.thestory.setup
 
 import com.github.rskupnik.thestory.application.API
+import com.github.rskupnik.thestory.application.internal.InternalsContainer
 import com.github.rskupnik.thestory.external.feedback.CallbackReceiver
 import com.github.rskupnik.thestory.external.file.FileSaver
 import com.github.rskupnik.thestory.implementations.inmemory.InMemoryFileSaver
@@ -10,27 +11,27 @@ class ApplicationContext {
     private final API api
     private final CallbackReceiver callbackReceiver
     private final FileSaver fileSaver
-    private final Map<Class<Object>, Object> internals
+    private final InternalsContainer internalsContainer
 
     private ApplicationContext(API api, CallbackReceiver callbackReceiver, FileSaver fileSaver,
-                               Map<Class<Object>, Object> internals) {
+                               InternalsContainer internalsContainer) {
         this.api = api
         this.callbackReceiver = callbackReceiver
         this.fileSaver = fileSaver
-        this.internals = internals
+        this.internalsContainer = internalsContainer
     }
 
     public static ApplicationContext standardApplication(CallbackReceiver callbackReceiver) {
-        final Map<Class<Object>, Object> internals = new HashMap<>()
+        final InternalsContainer internalsContainer = new InternalsContainer()
 
-        def API api = Setup.standardApplication(internals)
+        def API api = Setup.standardApplication(internalsContainer)
         if (callbackReceiver != null)
             api.provideImplementation(CallbackReceiver.class, callbackReceiver)
 
         def fileSaver = new InMemoryFileSaver()
         api.provideImplementation(FileSaver.class, fileSaver)
 
-        return new ApplicationContext(api, callbackReceiver, fileSaver, internals)
+        return new ApplicationContext(api, callbackReceiver, fileSaver, internalsContainer)
     }
 
     void enableSpy(Class<Object> clazz) {
--- test/src/test/groovy/com/github/rskupnik/thestory/setup/Setup.groovy
+++ test/src/test/groovy/com/github/rskupnik/thestory/setup/Setup.groovy
@@ -2,21 +2,22 @@ package com.github.rskupnik.thestory.setup
 
 import com.github.rskupnik.thestory.application.API
 import com.github.rskupnik.thestory.application.Application
+import com.github.rskupnik.thestory.application.internal.InternalsContainer
 import com.github.rskupnik.thestory.external.asset.AssetLoader
 import com.github.rskupnik.thestory.external.file.FileLoader
 import com.github.rskupnik.thestory.implementations.classpath.ClasspathFileLoader
 import com.github.rskupnik.thestory.implementations.dummy.DummyAssetLoader
 
 class Setup {
-    static API application(FileLoader fileLoader, AssetLoader assetLoader, Map<Class<Object>, Object> internals = null) {
-        final API api = Application.INSTANCE.init(internals)
+    static API application(FileLoader fileLoader, AssetLoader assetLoader, InternalsContainer internalsContainer = null) {
+        final API api = Application.INSTANCE.init(internalsContainer)
         api.provideImplementation(FileLoader.class, fileLoader)
         api.provideImplementation(AssetLoader.class, assetLoader)
 
         return api
     }
 
-    static API standardApplication(Map<Class<Object>, Object> internals = null) {
-        return application(new ClasspathFileLoader(), new DummyAssetLoader(), internals)
+    static API standardApplication(InternalsContainer internalsContainer = null) {
+        return application(new ClasspathFileLoader(), new DummyAssetLoader(), internalsContainer)
     }
 }
