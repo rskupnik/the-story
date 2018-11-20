0c456c4b4e310d742892553916a3d176c3bbe1b2
Radosław Skupnik <rskupnik@gmail.com>
2018-11-20 11:38:33 +0100

    Introduce ServiceInitializer

---
+++ core/persistence/initializer/src/main/kotlin/com/github/rskupnik/thestory/persistence/init/PersistenceInitializer.kt
@@ -0,0 +1,10 @@
+package com.github.rskupnik.thestory.persistence.init
+
+import com.github.rskupnik.thestory.shared.ServiceInitializer
+
+class PersistenceInitializer : ServiceInitializer {
+
+    override fun init() {
+
+    }
+}
---
+++ core/shared/src/main/kotlin/com/github/rskupnik/thestory/shared/ServiceInitializer.kt
@@ -0,0 +1,5 @@
+package com.github.rskupnik.thestory.shared
+
+interface ServiceInitializer {
+    fun init()
+}
