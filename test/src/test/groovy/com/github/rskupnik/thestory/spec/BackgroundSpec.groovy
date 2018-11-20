430c1dddaecb02890deb07f99281d6334c6bc52a
Radosław Skupnik <rskupnik@gmail.com>
2018-11-20 10:34:55 +0100

    Update existing test and add new one for normal mapped background

--- test/src/test/groovy/com/github/rskupnik/thestory/spec/BackgroundSpec.groovy
+++ test/src/test/groovy/com/github/rskupnik/thestory/spec/BackgroundSpec.groovy
@@ -1,22 +1,39 @@
 package com.github.rskupnik.thestory.spec
 
+import com.github.rskupnik.thestory.api.command.details.background.NormalMappedBackgroundDetails
 import com.github.rskupnik.thestory.background.BackgroundService
 import com.github.rskupnik.thestory.external.feedback.CallbackReceiver
 import com.github.rskupnik.thestory.setup.ApplicationContext
 
 class BackgroundSpec extends AbstractSpec {
 
-    // TODO: Test setting background - need access to internal classes for tests first
-    def "should set background"() {
+    def "should set background: no background"() {
         given:
         def app = ApplicationContext.standardApplication(Mock(CallbackReceiver))
         def spy = enableSpy(app, BackgroundService.class)
 
         when:
+        app.api.commandAPI.initializeGame("demo")
         app.api.commandAPI.setBackground(null)
 
         then:
         1 * ((BackgroundService)spy).setNoBackground()
         0 * ((BackgroundService)spy).setNormalMappedBackground(_, _)
+    }
+
+    def "should set background: normal-mapped background"() {
+        given:
+        def app = ApplicationContext.standardApplication(Mock(CallbackReceiver))
+        def spy = enableSpy(app, BackgroundService.class)
+        def image = "image"
+        def normalImage = "normalImage"
+
+        when:
+        app.api.commandAPI.initializeGame("demo")
+        app.api.commandAPI.setBackground(new NormalMappedBackgroundDetails(image, normalImage))
+
+        then:
+        0 * ((BackgroundService)spy).setNoBackground()
+        1 * ((BackgroundService)spy).setNormalMappedBackground(image, normalImage)
     }
 }
