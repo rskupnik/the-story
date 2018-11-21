b5d8044b4219a9ce19a77e45cc87cdf54fbddbd0
Myzreal <r.skupnik@gmail.com>
2018-11-21 20:50:23 +0100

    Add GameState domain and GamePhase

--- core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/GameStateService.kt
+++ core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/GameStateService.kt
@@ -1,3 +1,8 @@
 package com.github.rskupnik.thestory.gameState
 
-interface GameStateService
+import com.github.rskupnik.thestory.gamestate.domain.GamePhase
+
+interface GameStateService {
+
+    fun setPhase(phase: GamePhase)
+}
--- core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/internal/DefaultGameStateService.kt
+++ core/gameState/src/main/kotlin/com/github/rskupnik/thestory/gameState/internal/DefaultGameStateService.kt
@@ -1,7 +1,12 @@
 package com.github.rskupnik.thestory.gameState.internal
 
 import com.github.rskupnik.thestory.gameState.GameStateService
+import com.github.rskupnik.thestory.gamestate.domain.GamePhase
 
 internal class DefaultGameStateService : GameStateService {
     private val gameState = GameState()
+
+    override fun setPhase(phase: GamePhase) {
+        gameState.phase = phase
+    }
 }
