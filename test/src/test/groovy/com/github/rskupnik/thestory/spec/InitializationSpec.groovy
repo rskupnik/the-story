34cf2c9b31cac9e78233f61c681516c17766974a
Radosław Skupnik <rskupnik@gmail.com>
2018-11-22 12:20:21 +0100

    Add a test for initializing more than once

--- test/src/test/groovy/com/github/rskupnik/thestory/spec/InitializationSpec.groovy
+++ test/src/test/groovy/com/github/rskupnik/thestory/spec/InitializationSpec.groovy
@@ -1,5 +1,6 @@
 package com.github.rskupnik.thestory.spec
 
+import com.github.rskupnik.thestory.domain.module.ModuleService
 import com.github.rskupnik.thestory.gamestate.domain.GamePhase
 import com.github.rskupnik.thestory.setup.ApplicationContext
 
@@ -8,23 +9,27 @@ class InitializationSpec extends AbstractSpec {
     def "should initialize game without errors"() {
         given:
         def app = ApplicationContext.standardApplication()
+        def spy = enableSpy(app, ModuleService.class)
 
         when:
         app.api.commandAPI.initializeGame("demo")
 
         then:
+        1 * ((ModuleService)spy).load("demo")
         app.api.queryAPI.getCurrentGamePhase() == GamePhase.RUNNING
     }
 
     def "should only initialize once"() {
         given:
         def app = ApplicationContext.standardApplication()
+        def spy = enableSpy(app, ModuleService.class)
 
         when:
         app.api.commandAPI.initializeGame("demo")
         app.api.commandAPI.initializeGame("demo")
 
         then:
+        1 * ((ModuleService)spy).load("demo")   // Only once
         app.api.queryAPI.getCurrentGamePhase() == GamePhase.RUNNING
     }
 }
