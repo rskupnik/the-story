0336fa6118b1bd26e0f3fedd1a507906a0c419f7
Radosław Skupnik <rskupnik@gmail.com>
2018-11-23 12:48:00 +0100

    Add an irrelevant TODO (AbstractActionHandler)

--- core/action/src/main/kotlin/com/github/rskupnik/thestory/domain/action/AbstractActionHandler.kt
+++ core/action/src/main/kotlin/com/github/rskupnik/thestory/domain/action/AbstractActionHandler.kt
@@ -1,5 +1,6 @@
 package com.github.rskupnik.thestory.domain.action
 
+// TODO: Should this be moved to :core:action:handlers?
 abstract class AbstractActionHandler(
         executor: ActionExecutor
 ) : ActionHandler {
