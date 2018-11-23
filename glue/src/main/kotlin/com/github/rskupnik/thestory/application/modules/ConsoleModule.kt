2a7cdb898b21f7f4e39374df0f62f707bb36352c
Radosław Skupnik <rskupnik@gmail.com>
2018-11-23 13:06:53 +0100

    Add ConsoleModule to injection mechanism

---
+++ glue/src/main/kotlin/com/github/rskupnik/thestory/application/modules/ConsoleModule.kt
@@ -0,0 +1,21 @@
+package com.github.rskupnik.thestory.application.modules
+
+import com.github.rskupnik.thestory.core.console.ConsoleInjectorHandle
+import com.github.rskupnik.thestory.core.console.ConsoleService
+import com.github.rskupnik.thestory.core.console.handler.ConsoleCommandInjectorHandle
+import com.github.rskupnik.thestory.core.console.handler.PrintCommandHandler
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+internal class ConsoleModule {
+
+    @Provides @Singleton
+    fun service(): ConsoleService = ConsoleInjectorHandle.service()
+
+    @Provides @Singleton
+    fun printCommandHandler(
+            consoleService: ConsoleService
+    ): PrintCommandHandler = ConsoleCommandInjectorHandle.printCommandHandler(consoleService)
+}
