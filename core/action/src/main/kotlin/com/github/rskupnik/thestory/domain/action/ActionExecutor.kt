bfefeb0fc1ea00331e3d86b6657498580fc61455
Myzreal <r.skupnik@gmail.com>
2018-11-23 20:44:28 +0100

    Add kdocs for :core:action

--- core/action/src/main/kotlin/com/github/rskupnik/thestory/domain/action/ActionExecutor.kt
+++ core/action/src/main/kotlin/com/github/rskupnik/thestory/domain/action/ActionExecutor.kt
@@ -1,5 +1,14 @@
 package com.github.rskupnik.thestory.domain.action
 
+/**
+ * Executes actions by delegating to [ActionHandler]s registered for the specific id
+ */
 interface ActionExecutor {
+
+    /**
+     * Register an [ActionHandler] for the [id].
+     *
+     * The [handler] will be executed for the action identified by [id].
+     */
     fun register(id: String, handler: ActionHandler)
 }
--- core/action/src/main/kotlin/com/github/rskupnik/thestory/domain/action/ActionHandler.kt
+++ core/action/src/main/kotlin/com/github/rskupnik/thestory/domain/action/ActionHandler.kt
@@ -3,8 +3,18 @@ package com.github.rskupnik.thestory.domain.action
 import com.github.rskupnik.thestory.action.domain.Action
 import com.github.rskupnik.thestory.shared.entity.EntityId
 
+/**
+ * A handler for a specific action.
+ */
 interface ActionHandler {
+
+    /**
+     * [String] that identifies the action to be handled.
+     */
     fun id(): String
 
+    /**
+     * Handle the [action] launched for the specific entity ([entityId]) with external [data] provided.
+     */
     fun handle(action: Action, entityId: EntityId?, data: Map<String, Any>?)
 }
