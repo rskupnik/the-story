d56ab796105300fb083f120beaa87ff0e5974720
Radosław Skupnik <rskupnik@gmail.com>
2018-11-23 12:51:06 +0100

    Add register() function stub to ConsoleService

--- core/console/src/main/kotlin/com/github/rskupnik/thestory/core/console/ConsoleService.kt
+++ core/console/src/main/kotlin/com/github/rskupnik/thestory/core/console/ConsoleService.kt
@@ -2,5 +2,7 @@ package com.github.rskupnik.thestory.core.console
 
 interface ConsoleService {
 
+    fun register(handler: CommandHandler)
+
     fun execute(id: String, params: Array<Any>?)
 }
--- core/console/src/main/kotlin/com/github/rskupnik/thestory/core/console/DefaultConsoleService.kt
+++ core/console/src/main/kotlin/com/github/rskupnik/thestory/core/console/DefaultConsoleService.kt
@@ -2,6 +2,10 @@ package com.github.rskupnik.thestory.core.console
 
 internal class DefaultConsoleService : ConsoleService {
 
+    override fun register(handler: CommandHandler) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun execute(id: String, params: Array<Any>?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
