209f3c50bb5c61598df4b424f44c176b5907f7dc
Radosław Skupnik <rskupnik@gmail.com>
2018-11-23 13:59:38 +0100

    Initializer docs

--- api/src/main/kotlin/com/github/rskupnik/thestory/api/init/Initializer.kt
+++ api/src/main/kotlin/com/github/rskupnik/thestory/api/init/Initializer.kt
@@ -3,6 +3,12 @@ package com.github.rskupnik.thestory.api.init
 import com.github.rskupnik.thestory.persistence.init.PersistenceInitializer
 import com.github.rskupnik.thestory.shared.ServiceInitializer
 
+/**
+ * This class is used when starting the application to allow the core code to do some initializing.
+ *
+ * The application layer that provides dependency injection should inject initializers into this class that it wants
+ * to initialize at the start.
+ */
 class Initializer(
         private val initializers: Array<ServiceInitializer>
 ) {
