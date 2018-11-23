bcfb59275733af880dcd74dbc2ee1c8b79adee91
Radosław Skupnik <rskupnik@gmail.com>
2018-11-23 13:57:52 +0100

    NormalMappedBackgroundDetails docs

--- api/src/main/kotlin/com/github/rskupnik/thestory/api/command/details/background/NormalMappedBackgroundDetails.kt
+++ api/src/main/kotlin/com/github/rskupnik/thestory/api/command/details/background/NormalMappedBackgroundDetails.kt
@@ -1,5 +1,8 @@
 package com.github.rskupnik.thestory.api.command.details.background
 
+/**
+ * Details of a normal-mapped background to be set.
+ */
 data class NormalMappedBackgroundDetails(
         val image: String,
         val normalImage: String
