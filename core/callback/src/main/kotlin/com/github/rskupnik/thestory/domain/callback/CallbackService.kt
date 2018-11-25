f4d32645241a644abced0db17d8027ad8b404f49
Myzreal <r.skupnik@gmail.com>
2018-11-25 16:44:14 +0100

    Add :core:callback docs

--- core/callback/src/main/kotlin/com/github/rskupnik/thestory/domain/callback/CallbackService.kt
+++ core/callback/src/main/kotlin/com/github/rskupnik/thestory/domain/callback/CallbackService.kt
@@ -3,6 +3,11 @@ package com.github.rskupnik.thestory.domain.callback
 import com.github.rskupnik.thestory.core.callback.domain.Callback
 import com.github.rskupnik.thestory.shared.entity.EntityId
 
+/**
+ * Contains logic to handle [Callback]s.
+ *
+ * A callback is basically a list of actions that execute at specific events, such as removing an item from equipment.
+ */
 interface CallbackService {
     fun execute(callback: Callback, entityId: EntityId)
 }
