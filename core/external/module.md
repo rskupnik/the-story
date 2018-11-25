359cf7037c7d6db4e63744d7c6c00147e08c9a7a
Myzreal <r.skupnik@gmail.com>
2018-11-25 17:02:53 +0100

    Add docs for :core:external

---
+++ core/external/module.md
@@ -0,0 +1,5 @@
+# Module core:external
+
+Contains interfaces for all external dependencies that need to be satisfied by the higher level layers.
+
+The core app uses these dependencies as interfaces and the real implementations need to be filled in at runtime.
--- core/external/src/main/kotlin/com/github/rskupnik/thestory/external/dto/OptionLabel.kt
+++ core/external/src/main/kotlin/com/github/rskupnik/thestory/external/dto/OptionLabel.kt
@@ -3,6 +3,7 @@ package com.github.rskupnik.thestory.external.dto
 import com.github.rskupnik.thestory.shared.Context
 import com.github.rskupnik.thestory.shared.entity.EntityId
 
+// TODO: Move to feedback?
 data class OptionLabel(
         val entityId: EntityId,
         val label: String,
