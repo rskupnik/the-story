0997edf5044d8bce35444242dab31b07489422d8
Myzreal <r.skupnik@gmail.com>
2018-11-25 16:47:42 +0100

    Update docs on :core:console

--- core/console/src/main/kotlin/com/github/rskupnik/thestory/core/console/CommandHandler.kt
+++ core/console/src/main/kotlin/com/github/rskupnik/thestory/core/console/CommandHandler.kt
@@ -5,7 +5,13 @@ package com.github.rskupnik.thestory.core.console
  */
 interface CommandHandler {
 
+    /**
+     * A String representing the command this handler will handle.
+     */
     fun id(): String
 
+    /**
+     * Main handling logic resides here.
+     */
     fun handle(params: Array<String>)
 }
