3843f93543a00a70924518be31342fe703a96073
Myzreal <r.skupnik@gmail.com>
2018-11-25 16:54:48 +0100

    Add docs for :core:event

--- core/event/src/main/kotlin/com/github/rskupnik/thestory/event/EventDispatcher.kt
+++ core/event/src/main/kotlin/com/github/rskupnik/thestory/event/EventDispatcher.kt
@@ -2,9 +2,18 @@ package com.github.rskupnik.thestory.event
 
 import kotlin.reflect.KClass
 
+/**
+ * Handles sending and retrieving events among the core application.
+ */
 interface EventDispatcher {
 
+    /**
+     * Dispatch an [event].
+     */
     fun dispatch(event: Event)
 
+    /**
+     * Register a [handler] for a particular [klass] of an event.
+     */
     fun register(klass: KClass<out Event>, handler: (Event) -> Unit)
 }
